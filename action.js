17ab3cb038f52b99e16f2f5fab3549d57f083948
Naeem <naeemkarimzadeh@gmail.com>
2024-08-08 17:20:50 +0330

    css & html done

---
+++ action.js
@@ -0,0 +1,15 @@
+//------------------ select elements ------------------
+let plus_button = document.getElementById("btn-increase");
+let minus_button = document.getElementById("btn-decrease");
+let reset_button = document.getElementById("btn-reset");
+
+let set_limit_button = document.getElementById("limit-button");
+
+let limit_input_text = document.getElementById("limit-input");
+let text_result = document.getElementById("text-result");
+
+//------------------------------------
+
+let number ;
+number = parseInt(text_result.value);
+
