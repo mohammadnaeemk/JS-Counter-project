1bbdcd7da47b26317280e31cd6f6be4a341f3f28
Naeem <naeemkarimzadeh@gmail.com>
2024-08-10 18:02:59 +0330

    Adding the number and setting the limit has been completed successfully.

--- action.js
+++ action.js
@@ -9,7 +9,42 @@ let limit_input_text = document.getElementById("limit-input");
 let text_result = document.getElementById("text-result");
 
 //------------------------------------
+let number,
+  limit = 10;
 
-let number ;
-number = parseInt(text_result.value);
+set_limit_button.onclick = function () {
+  if (parseInt(limit_input_text.value) >= 0) {
+    limit = parseInt(limit_input_text.value);
+    document.querySelector("#alert4").classList.add("show", 40);
+    setTimeout(function () {
+      document.querySelector("#alert4").classList.remove("show");
+    }, 1000);
 
+
+  }else if(limit_input_text.value == ""){
+    document.querySelector("#alert3").classList.add("show", 40);
+    setTimeout(function () {
+        
+      document.querySelector("#alert3").classList.remove("show");
+    }, 1000);
+  }
+   else {
+
+    document.querySelector("#alert2").classList.add("show", 40);
+    setTimeout(function () {
+      document.querySelector("#alert2").classList.remove("show");
+    }, 1000);
+
+  }
+};
+
+plus_button.onclick = function () {
+  if (parseInt(text_result.value) + 1 <= limit) {
+    ++text_result.value;
+  } else {
+    document.querySelector("#alert1").classList.add("show", 40);
+    setTimeout(function () {
+      document.querySelector("#alert1").classList.remove("show");
+    }, 1000);
+  }
+};
